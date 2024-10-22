53f9cd1b88165cb0b906dc159743e017a73c537d
Sergey Ustinov <ustinov.sergey.st@gmail.com>
2024-10-23 01:27:49 +0300

    Update Even.java (ready to go)

--- app/src/main/java/hexlet/code/Even.java
+++ app/src/main/java/hexlet/code/Even.java
@@ -5,17 +5,56 @@ import java.util.Scanner;
 public class Even {
     public static void launchEven() {
 
-        Greet.greetUser();
+        System.out.println("\nWelcome to the Brain Games!");
 
-        int randomNumber = (int) Math.round(Math.random() * 100);
+        Scanner userNameSelection = new Scanner(System.in);
 
-        System.out.println("""
-                           Answer 'yes' if the number is even, otherwise answer 'no'.
-                           Question:""" + " " + randomNumber);
+        System.out.print("May I have your name? ");
+        String userName = userNameSelection.next();
+        System.out.println("Hello, " + userName + "!");
+
+        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
 
         Scanner evenOrOddSelection = new Scanner(System.in);
-        System.out.print("Your answer: ");
-        String playerAnswer = evenOrOddSelection.next();
 
+        var correctCount = 0;
+        var countToWin = 3;
+
+        while (correctCount < countToWin) {
+
+            int randomNumber = (int) Math.round(Math.random() * 100);
+
+            System.out.println("Question: " + randomNumber);
+            System.out.print("Your answer: ");
+            String playerAnswer = evenOrOddSelection.next();
+
+            if (randomNumber % 2 == 0 && playerAnswer.equals("yes")) {
+                System.out.println("Correct!");
+                correctCount++;
+
+            } else if (randomNumber % 2 != 0 && playerAnswer.equals("no")) {
+                System.out.println("Correct!");
+                correctCount++;
+
+            } else {
+                switch (playerAnswer) {
+
+                    case "yes":
+                        System.out.println("'yes' is wrong answer ;(. Correct answer was 'no'.");
+                        System.out.println("Let's try again, " + userName + "!");
+                        return;
+
+                    case "no":
+                        System.out.println("'no' is wrong answer ;(. Correct answer was 'yes'.");
+                        System.out.println("Let's try again, " + userName + "!");
+                        return;
+
+                    default:
+                        System.out.println("Please answer 'yes' or 'no' only.");
+                        System.out.println("Let's try again, " + userName + "!");
+                        return;
+                }
+            }
+        } System.out.println("Congratulations, " + userName + "!");
     }
 }
